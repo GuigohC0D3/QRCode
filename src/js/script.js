2f65e530d8ac381509d432e906deac7500eeee54
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2024-05-24 12:55:37 -0300

    Gerador de QR Code

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+function generateQrCode() {
+  const qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerText = "Gerando codigo....";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Codigo criado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateQrCode();
+  }
+});
+
+// Limpar area do QR Code
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
